7615b8dde6cb5e7101b7a8e6a5f0634bab18850a
Kiruthika Velusamy <kiruthika94velusamy@gmail.com>
2016-05-02 14:40:50 +0530

    Added files via upload

---
+++ turtle_triangle.py
@@ -0,0 +1,24 @@
+import turtle
+def draw_square():
+    brad=turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("red")
+    window=turtle.Screen()
+    window.bgcolor("yellow")
+    brad.forward(100)
+    brad.right(120)
+    brad.forward(100)
+    brad.right(120)
+    brad.forward(100)
+    brad.right(120)
+    brad.forward(100)
+    brad.right(120)
+    brad.forward(100)
+    brad.right(120)
+    brad.forward(100)
+    brad.right(120)
+    brad.forward(100)
+    window.exitonclick()
+    
+
+draw_square()
